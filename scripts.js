7747d39798182b9cc99ec3ca71740bad07fe6cfb
hnitch <hnitchx@gmail.com>
2021-12-17 11:58:54 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,38 @@
+$("#div1").mouseenter(function() {
+    var $div2 = $("#div2");
+    if ($div2.is(":visible")) { return; }
+    $div2.show();
+    setTimeout(function() {
+        $div2.hide();
+    }, 5000);
+});
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
